15497ff33e19f8e857514942068d0936b3bfc3e4
Tamis van der Laan <tamis.vanderlaan@bisc.nl>
2014-03-01 18:09:37 +0100

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -11,24 +11,30 @@
 using namespace std;
 
 int main(int argc, char* args[])
-{
-	//ss(10,3);
-	
-	//#pragma omp parallel for
-	for(int i = 2; i < 5000; i++)
+{	
+	for(int i = 2; i < 6000; i++)
 	{
-//		bool aks_prime = aks(i);
-//		if(aks_prime)
-//			cout << "AKS    : " << i << " is a prime" << endl;
-//		else
-//			cout << "AKS    : " << i << " is not a prime" << endl;
-
-		bool mr_prime = mr_par(i,1000);
+		bool aks_prime = aks(i);
+		if(aks_prime)
+			cout << "AKS     : " << i << " is a prime" << endl;
+		else
+			cout << "AKS     : " << i << " is not a prime" << endl;
+	}
+	for(int i = 2; i < 6000; i++)
+	{
+		bool mr_prime = mr(i,2500);
 		if(mr_prime)
 			cout << "MR     : " << i << " is a prime" << endl;
 		else
 			cout << "MR     : " << i << " is not a prime" << endl;
+	}
+	for(int i = 2; i < 6000; i++)
+	{
+		bool mr_par_prime = mr_par(i,2500);
+		if(mr_par_prime)
+			cout << "MR_par     : " << i << " is a prime" << endl;
+		else
+			cout << "MR_par     : " << i << " is not a prime" << endl;
 	}	
 	return 0;
-	
 }
--- src/mr.cpp
+++ src/mr.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <math.h>
 #include <cstdlib>
-#include <ctime>
+#include <sys/time.h>
 #include <omp.h>
 //#include "util.cpp"
 
@@ -18,13 +18,18 @@ int modPow(int a, int b, int c)
 	return res % c;
 }
 
+void seed_rand()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    srand(tv.tv_sec * tv.tv_usec);
+}
+
 bool mr_par(int n, int it)
 {
 	if(n == 2 || n == 3) return true;
-
 	if(n < 2 || (n != 2 && n%2==0)) return false;
-	
-	srand((unsigned)time(0));
+	seed_rand();
 
 	int s = n-1;
 	while(s%2 == 0)
@@ -37,14 +42,14 @@ bool mr_par(int n, int it)
 		#pragma omp flush (ans)
 		if(ans)
 		{
-			int r = rand();
-			int a = (r%(n-1))+1;
-			int temp = s;
-			int mod = modPow(a, temp, n);
+			int r 		= rand();
+			int a 		= (r%(n-1))+1;
+			int temp 	= s;
+			int mod 	= modPow(a, temp, n);
 			while(temp!=n-1 && mod!=1 && mod!=n-1)
 			{
-				mod = (mod*mod)%n;
-				temp = 2 * temp;
+				mod	= (mod*mod)%n;
+				temp	= 2 * temp;
 			}
 			if(mod!=n-1 && temp%2==0)
 			{
@@ -52,7 +57,6 @@ bool mr_par(int n, int it)
 				#pragma omp flush (ans)
 			}
 		}
-
 	}
 	return ans;
 }
@@ -60,10 +64,9 @@ bool mr_par(int n, int it)
 bool mr(int n, int it)
 {
 	if(n == 2 || n == 3) return true;
-
 	if(n < 2 || (n != 2 && n%2==0)) return false;
 	
-	srand((unsigned)time(0));
+	seed_rand();
 
 	int s = n-1;
 	while(s%2 == 0)
