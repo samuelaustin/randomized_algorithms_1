7571cccfacffe7f1a90409c3bf72318ddb3226cc
Samuel Austin <samuelaustin_3@hotmail.com>
2014-03-01 16:13:42 +0100

    editmain

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <math.h>
+#include <unistd.h>
 #include "aks.cpp"
+#include "mr.cpp"
 
 using namespace std;
 
@@ -14,12 +16,19 @@ int main(int argc, char* args[])
 		else
 			cout << "AKS    : " << i << " is not a prime" << endl;
 
-		bool is_prime = IsPrime(i);
-		
-		if(is_prime)
-			cout << "isPrime: " << i << " is a prime" << endl;
+		bool mr_prime = mr(i, 1);
+		if(mr_prime)
+			cout << "MR     : " << i << " is a prime" << endl;
 		else
-			cout << "isPrime: " << i << " is not a prime" << endl;
+			cout << "MR     : " << i << " is not a prime" << endl;
+
+		//usleep(1000000);
+		//bool is_prime = IsPrime(i);
+		
+		//if(is_prime)
+		//	cout << "isPrime: " << i << " is a prime" << endl;
+		//else
+		//	cout << "isPrime: " << i << " is not a prime" << endl;
 	}	
 	return 0;
 }
