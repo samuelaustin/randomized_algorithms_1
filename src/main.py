a2f27e6f79cda39c89af66b6fe7c1a2c18b20325
Tamis van der Laan <tamis.vanderlaan@bisc.nl>
2014-03-04 16:02:21 +0100

    nice plots

--- src/main.py
+++ src/main.py
@@ -6,71 +6,80 @@ import timeit
 def time_func(it,func,*args):
 	t=timeit.Timer(lambda: func(*args))
 	return t.timeit(number=it)/it
+	return data 
 
 def test_isPrime(i, j):
 	RANGE 	= range(i,j)
 	data 	= numpy.zeros(len(RANGE))
 	print("isPrime")
 	for i in range(0, len(RANGE)):
-		data[i]		= time_func(5,prime.isPrime,RANGE[i])
-	plot.plot(RANGE,data,label="isPrime(n)")
-	plot.legend()	
-	plot.show()
+		data[i]	= time_func(5,prime.isPrime,RANGE[i])
+	plot.plot(RANGE,data,'+',label="isPrime(n)")
+	return data 
 
-def test_aks(i, j, k):
+def test_aks(i, j):
 	RANGE 	= range(i,j)
 	data 	= numpy.zeros(len(RANGE))
 	print("aks")
 	for i in range(0, len(RANGE)):
-		data[i]		= time_func(5,prime.aks,RANGE[i])
-	plot.plot(RANGE,data,label="aks(n)")
-	plot.legend()	
-	plot.show()
+		data[i]	= time_func(5,prime.aks,RANGE[i])
+	plot.plot(RANGE,data,'+',label="aks(n)")
+	return data 
 
 def test_ss(i, j, k):
 	RANGE 	= range(i,j)
 	data 	= numpy.zeros(len(RANGE))
 	print("ss")
 	for i in range(0, len(RANGE)):
-		data[i]		= time_func(5,prime.ss,RANGE[i], k)
-	plot.plot(RANGE,data,label="ss(n," + str(k) + ")")
-	plot.legend()	
-	plot.show()
+		data[i]	= time_func(5,prime.ss,RANGE[i], k)
+	plot.plot(RANGE,data,'+',label="ss(n," + str(k) + ")")
+	return data 
 
 def test_ss_par(i, j, k):
 	RANGE 	= range(i,j)
 	data 	= numpy.zeros(len(RANGE))
 	print("ss_par")
 	for i in range(0, len(RANGE)):
-		data[i]		= time_func(5,prime.ss_par,RANGE[i], k)
-	plot.plot(RANGE,data,label="isPrime(n," + str(k) + ")")
-	plot.legend()	
-	plot.show()
+		data[i]	= time_func(5,prime.ss_par,RANGE[i], k)
+	plot.plot(RANGE,data,'+',label="ss_par(n," + str(k) + ")")
+	return data 
 
 def test_mr(i, j, k):
 	RANGE 	= range(i,j)
 	data 	= numpy.zeros(len(RANGE))
 	print("mr")
 	for i in range(0, len(RANGE)):
-		data[i]		= time_func(5,prime.mr,RANGE[i], k)
-	plot.plot(RANGE,data,label="isPrime(n," + str(k) + ")")
-	plot.legend()	
-	plot.show()
+		data[i]	= time_func(5,prime.mr,RANGE[i], k)
+	plot.plot(RANGE,data,'+',label="mr(n," + str(k) + ")")
+	return data 
 
 def test_mr_par(i, j, k):
 	RANGE 	= range(i,j)
 	data 	= numpy.zeros(len(RANGE))
 	print("mr_par")
 	for i in range(0, len(RANGE)):
-		data[i]		= time_func(5,prime.mr_par,RANGE[i], k)
-	plot.plot(RANGE,data,label="isPrime(n," + str(k) + ")")
-	plot.legend()	
-	plot.show()
+		data[i]	= time_func(5,prime.mr_par,RANGE[i], k)
+	plot.plot(RANGE,data,'+',label="mr_par(n," + str(k) + ")")
+	return data 
 
+test_isPrime(2,1000)
+test_aks(2,1000)
+plot.legend()	
+plot.show()
 
-i = 2
-j = 60000
-k = 25
+test_mr(100000+2, 100000+60000, 50)
+test_mr_par(100000+2, 100000+60000, 50)
+plot.legend()	
+plot.show()
 
-test_ss_par(i, j, k)
+test_ss(100000+2, 100000+60000, 50)
+test_ss_par(100000+2, 100000+60000, 50)
+plot.legend()	
+plot.show()
 
+test_mr(100000+2, 100000+60000, 30)
+test_mr_par(100000+2, 100000+60000, 30)
+test_ss(100000+2, 100000+60000, 30)
+test_ss_par(100000+2, 100000+60000, 30)
+plot.legend()	
+plot.show()
