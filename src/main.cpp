27d3dc1a68f05f3342afaf20594800f897a9fc3b
Tamis van der Laan <tamis.vanderlaan@bisc.nl>
2014-03-05 12:49:58 +0100

    split main

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,9 @@
-#include <boost/python.hpp>
 #include <iostream>
 #include "aks.cpp"
 #include "mr.cpp"
 #include "ss.cpp"
 
 using namespace std;
-using namespace boost::python;
-
-BOOST_PYTHON_MODULE(prime)
-{
-	def("isPrime",isPrime);
-	def("aks",aks);
-	def("mr",mr);
-	def("mr_par",mr_par);
-	def("ss",ss);
-	def("ss_par",ss_par);
-}
-
 int main(int argc,char* args[])
 {	
 	for(int i=1; i<500000; i++)
---
+++ src/python_bindings.cpp
@@ -0,0 +1,15 @@
+#include <boost/python.hpp>
+#include "aks.cpp"
+#include "mr.cpp"
+#include "ss.cpp"
+
+using namespace boost::python;
+BOOST_PYTHON_MODULE(prime)
+{
+	def("isPrime",isPrime);
+	def("aks",aks);
+	def("mr",mr);
+	def("mr_par",mr_par);
+	def("ss",ss);
+	def("ss_par",ss_par);
+}
